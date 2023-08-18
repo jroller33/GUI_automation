adbecd7cef46db8fcd4789fa9fe0591e0bf6dee3
John Roller <jtroller33@gmail.com>
2023-08-18 14:33:13 -0500

    initial

---
+++ gui_auto.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+wh = pyautogui.size()
+
+# for i in range(2):
+#     pyautogui.moveTo(100, 100, duration=0.5)
+#     pyautogui.moveTo(200, 100, duration=0.5)
+#     pyautogui.moveTo(200, 200, duration=0.5)
+#     pyautogui.moveTo(100, 200, duration=0.5)
+
+# pyautogui.mouseInfo()
+
+pyautogui.click(1773,26)    # minimizes vscode window if it's maximized
