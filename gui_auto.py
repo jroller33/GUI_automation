936dcfbc803ed4d898c40d26bc2afaf2bbfc5855
John Roller <jtroller33@gmail.com>
2023-08-19 16:26:14 -0500

    .

--- gui_auto.py
+++ gui_auto.py
@@ -2,22 +2,14 @@ import pyautogui
 import subprocess
 import time
 
-# wh = pyautogui.size()
+wh = pyautogui.size()
 
-# for i in range(2):
-#     pyautogui.moveTo(100, 100, duration=0.5)
-#     pyautogui.moveTo(200, 100, duration=0.5)
-#     pyautogui.moveTo(200, 200, duration=0.5)
-#     pyautogui.moveTo(100, 200, duration=0.5)
+for i in range(2):
+    pyautogui.moveTo(100, 100, duration=0.5)
+    pyautogui.moveTo(200, 100, duration=0.5)
+    pyautogui.moveTo(200, 200, duration=0.5)
+    pyautogui.moveTo(100, 200, duration=0.5)
 
-# pyautogui.mouseInfo()
+pyautogui.mouseInfo()
 
-# pyautogui.click(1773,26)    # minimizes vscode window if it's maximized
-
-active_window = pyautogui.getActiveWindow()
-
-active_window.minimize()
-
-time.sleep(5)
-
-subprocess.Popen("start chrome", shell=True)    # opens a new Chrome window
+pyautogui.click(1773,26)    # minimizes vscode window if it's maximized
